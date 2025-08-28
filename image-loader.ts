e298d287c2306df3b1dcb1b0fda5e01ffcf71bbf
Jochem Bruijns <j.bruijns@fullstaq.com>
2025-08-28 13:24:32 +0200

    Fix logo's loading

--- image-loader.ts
+++ image-loader.ts
@@ -9,13 +9,5 @@ export default function cloudflareLoader({ src }: ImageLoaderProps) {
     return src;
   }
 
-  if (src === "/hero.png") {
-    return `https://imagedelivery.net/gwqtS4kafZruByi--g_VMg/fea201d8-402f-42d4-c779-9c3f6a069600/public?quality=100`;
-  }
-
-  if (src === "/logo.png") {
-    return `https://imagedelivery.net/gwqtS4kafZruByi--g_VMg/1c342f76-f1f0-405e-d68b-ff9f84d17a00/public?quality=100`;
-  }
-
   return `/${normalizeSrc(src)}`;
 }
